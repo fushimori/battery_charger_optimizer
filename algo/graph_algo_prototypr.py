ddc6f497fac32d85d5d64ad3190542c5433bf6b1
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2024-07-10 20:54:33 +0300

    Update graph_algo_prototypr.py

--- algo/graph_algo_prototypr.py
+++ algo/graph_algo_prototypr.py
@@ -1,12 +1,22 @@
-import sys
-import os
 import networkx as nx
 import matplotlib.pyplot as plt
 from math import radians, cos, sin, sqrt, atan2
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import graph_building as gb
 
-from generation import graph_building as gb
 
+def haversine(coord1, coord2):
+    R = 6371.0
+    lat1, lon1 = radians(coord1[0]), radians(coord1[1])
+    lat2, lon2 = radians(coord2[0]), radians(coord2[1])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
 
 
 def calculate_zone_charge(G):
@@ -22,7 +32,7 @@ def calculate_zone_charge(G):
 def replace_batteries(G, current_node, remaining_batteries):
     replacement_log = []
     for scooter in G.nodes[current_node]['scooters']:
-        if scooter['battery'] < 50 and remaining_batteries > 0:
+        if scooter['battery'] < 60 and remaining_batteries > 0:
             replacement_log.append((current_node, scooter['id'], scooter['battery']))
             scooter['battery'] = 100 
             remaining_batteries -= 1
@@ -76,10 +86,26 @@ def greedy_route_planning(G, start, battery_capacity=15):
         path.append(next_node)
         current_node = next_node
 
+        zone_charge = calculate_zone_charge(G)
+        if zone_charge >= 80:
+            path.append(start)  # Return to the start node
+            break
+
     return path, calculate_zone_charge(G), replacement_log
 
+def calculate_path_distance(G, path):
+    total_distance = 0
+    for i in range(len(path) - 1):
+        if G.has_edge(path[i], path[i+1]):
+            # print(path[i], path[i+1], G[path[i]][path[i+1]]['weight'])
+            total_distance += G[path[i]][path[i+1]]['weight']
+        else:
+            # print(path[i], path[i+1])
+            total_distance += haversine(G.nodes[path[i]]['pos'], G.nodes[path[i+1]]['pos'])
+    return total_distance
+
 # Load the graph
-G = nx.read_gml('../data/graph_range.gml')
+G = nx.read_gml('graph.gml')
 
 # Set the starting node
 start_node = 'CS_0'
@@ -87,9 +113,11 @@ path, zone_charge, replacement_log = greedy_route_planning(G, start_node)
 
 print(f"Optimal Path: {path}")
 print(f"Zone Charge: {zone_charge:.2f}%")
-print("\nReplacement Log:")
-for log in replacement_log:
-    print(f"Station: {log[0]}, Scooter ID: {log[1]}, Previous Battery: {log[2]}%")
+path_distance = calculate_path_distance(G, path)
+print(f"Path Distance: {path_distance}")
+# print("\nReplacement Log:")
+# for log in replacement_log:
+#     print(f"Station: {log[0]}, Scooter ID: {log[1]}, Previous Battery: {log[2]}%")
 
 # Visualize the graph and the route
 pos = nx.get_node_attributes(G, 'pos')
@@ -98,6 +126,7 @@ node_colors = ['red' if G.nodes[node]['type'] == 'charging_station' else 'blue' 
 plt.figure(figsize=(15, 10))
 nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=300, font_size=8)
 
+# Highlight the optimal path
 if path:
     path_edges = list(zip(path, path[1:]))
     nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='green', width=2)
